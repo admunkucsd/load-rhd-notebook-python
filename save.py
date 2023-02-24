9af9cf8d15d4ca66f73565a09f74056efa33f672
Allen Munk <admunk@ucsd.edu>
2023-02-24 11:29:57 -0800

    Change back to generic input name

--- save.py
+++ save.py
@@ -1,4 +1,4 @@
 from importrhdutilities import *
-filename = '09_25_2020_Asleep_200925_104612.rhd' # Change this variable to load a different data file
+filename = 'input.rhd' # Change this variable to load a different data file
 result, data_present = load_file(filename)
 result["amplifier_data"].transpose().tofile("output.dat") # Change this to whatever you would like the output file name to be
